c42c610b2a27bae0085d2a3dd90fee594d9c77d5
lilly <lilly1987@naver.com>
2020-08-24 20:27:41 +0900

    내 아이템을 외부 파일로 읽어올수 있도록 변경

--- EnterTheGungeon/EnterTheGungeon.Lilly.plugins.cs
+++ EnterTheGungeon/EnterTheGungeon.Lilly.plugins.cs
@@ -8,6 +8,8 @@ using System.Collections;
 using Dungeonator;
 using UnityEngine;
 using BepInEx;
+using Microsoft.VisualBasic.FileIO;
+using System.Text.RegularExpressions;
 
 namespace EnterTheGungeon.Lilly.plugins
 {
@@ -21,10 +23,13 @@ namespace EnterTheGungeon.Lilly.plugins
         public static bool ShowHide = false;
 
         public static string stringToEdit = "only num";
+        public static string myitem = "myitem.txt";
 
         void Awake()
         {
             UnityEngine.Debug.LogError("Lilly. Awake!");
+
+            SetMyItemMake();
 /*
             configGreeting = Config.Bind("General",   // The section under which the option is shown
                              "GreetingText",  // The key of the configuration option in the configuration file
@@ -62,10 +67,11 @@ namespace EnterTheGungeon.Lilly.plugins
                     SetItem();
                 }
 
-                if (GUI.Button(new Rect(25f + 170f, sz * ct++, 170f, 30f), "my item set"))
+                if (GUI.Button(new Rect(25f + 170f, sz * ct, 170f, 30f), "my item set"))
                 {
                     SetMyItem();
                 }
+                myitem = GUI.TextField(new Rect(25f+ 170f*2, sz * ct++, 170f, 30f), myitem);
 
                 if (GUI.Button(new Rect(25f, sz * ct++, 170f, 30f), "Give Junkan"))
                 {
@@ -289,8 +295,226 @@ namespace EnterTheGungeon.Lilly.plugins
             }
         }
 
+        public static void SetMyItemMake()
+        {
+            FileInfo fileInfo = new FileInfo(myitem);
+            //파일 있는지 확인 있을때(true), 없으면(false)
+            if (!fileInfo.Exists)
+            {
+                using (FileStream fs = new FileStream(myitem, FileMode.Create))
+                {
+                    using (StreamWriter sr = new StreamWriter(fs))
+                    {
+                        sr.Write(
+                            @"
+                131//유틸리티 벨트
+				,273//레이저 조준기
+				,134 //탄띠
+				,634//크라이시스 스톤
+				,285//+1 총탄
+				,113//로켓 추진식 총탄
+				,298//전격탄
+				,638 //디볼버 라운드
+				,640 //보팔 총탄
+				,655 //배고픈 총탄
+				,288 //반사식 총탄
+				,304 //폭발성 탄약
+				,373 //알파 총탄
+				,374 //오메가 총탄
+				,204 //방사능 처리 납
+				,295 //뜨거운 총탄
+				,410 //배터리 총탄
+				,278 //서리 총탄
+				,527//매력탄
+				,533 //매력탄
+				//, //공포탄 총탄
+				,284// 자동유도 탄환
+				,352//그림자 탄환
+				,375 //간편 재장전 총탄
+				,635 //스노우총탄
+				//,//원격 총탄
+				,528 //좀비탄
+				//대공포탄
+				,538// 은탄
+				,532//도금 총탄
+				,627//백금 총알
+				,569//혼돈 탄환
+				//,521// 확률탄
+				,630//붕총탄
+				,114//바이오닉 레그
+				,427//산탄총 커피
+				,426//산탄 콜라
+				,212//탄도 장화
+				,110//매직 스위트
+				,187//요령 좋은 성격
+				,435//콧털
+				,213//리치의 집게손가락
+				,353//분노의 사진
+				,115//투표 용지
+				,414//인간 탄환
+				,354//군사 훈련
+				,463//쥐갈공명의 반지
+				,112//지도 제작자의 반지
+				,158//구덩이 군주의 부적
+				,191//화염 저항의 반지
+				,495//총기옥
+				,488//상자 집착증 반지
+				,309//클로런시 반지
+				,174//상자 우정 반지
+				,254//상자 우정 반지
+				,294//흉내내기 우정 반지
+				,456//방아쇠 반지
+				,159//건드로메다 질병
+				,258//브로콜리
+				,431//발키리 용액
+				,167//핏빛 눈알
+				,160//건나이트 투구
+				,161//건나이트 정강이받이
+				,162//건나이트 건틀렛
+				,163//건나이트 갑옷
+				,219//늙은 기사의 방패
+				,222//늙은 기사의 투구
+				,305//오래된 문장
+				,457//가시 갑옷
+				,564//풀메탈재킷
+				,256//무거운 장화
+				,193//맹독충 장화
+				,315//건부츠
+				,526//뿅뿅 부츠
+				//,667//쥐 장화. 엘리베이터 불가
+				,214//동전 왕관
+				,165//기름칠한 실린더
+				,170//아이스큐브
+				,190//롤링 아이
+				,135//전쟁의 톱니바퀴
+				,119//메트로놈
+				,138//벌집
+				,137//지도
+				,281//총굴 청사진
+				,253//총굴 고추
+				,259//항체
+				,262//화이트 구온석
+				,263//오렌지 구온석
+				,264//클리어 구온석
+				,466//그린 구온석
+				,260//핑크 구온석
+				,269//레드 구온석
+				,270//블루 구온석
+				,565//유리 구온석
+				//핫뜨거 손목시계
+				,280//드럼 클립
+				,287//백업용 총
+				,290//선글라스
+				,293//흉내내기 이빨 목걸이
+				,307//왁스 날개
+				,312//블래스트 투구
+				,313//몬스터 혈액
+				,314//나노머신
+				,289//칠 잎 클로버
+				,326//이인자
+				,321//황금 탄환 부적
+				,325//혼돈 탄환 부적
+				,322//로드스톤 탄환 부적
+				,342//우라늄 탄환 부적
+				,343//구리 탄환 부적
+				,344//서리 탄환 부적
+				,396//테이블 기술 - 마음의 눈
+				,397//테이블 기술 - 자금
+				,398//테이블 기술 - 로켓
+				//,//테이블 기술 - 산탄총
+				//,321//테이블 기술 - 히트
+				,399//테이블 기술 - 분노
+				,400//테이블 기술 - 공포탄
+				,465//테이블 기술 - 기절
+				,421//하트 권총집
+				,423//하트 목걸이
+				,424//하트 보틀
+				,425//하트 지갑
+				,440//루비 팔찌
+				,409//고장 난 텔레비전
+				,364//얼음 심장
+				,255//고대 영웅의 반다나
+				,436//피묻은 목도리
+				,437//근이완제
+				,500//힙 홀스터
+				,311//복제인간
+				//,452//스펀지
+				,453//방독면
+				,454//방호복
+				,487//상자 해부학
+				,166//쉘레톤 열쇠
+				,490//금전 벽돌
+				,529//전투 깃발
+				,491//조력자
+				,492//늑대
+				,300//개
+				,249//올빼미
+				,301//슈퍼 스페이스 터틀
+				,127//뻥이에요
+				,148//고물
+				,641//황금 고물
+				,580//쓰레기
+				,318//R2G2
+				,442//배지
+				,572//닭피리
+				,232//우주의 친구
+				,451//돼지→영웅 돼지
+				,632//칠면조
+				,664//어린 흉내쟁이
+				,461//공포탄 동료의 반지
+				,493//현금 가방
+				,494//은하 용맹 훈장
+				,434//총탄 우상
+				,271//납 어레미
+				,570//노란 약실.저주2
+"
+                            );
+                    }
+                }
+            }
+        }
+
         public static void SetMyItem()
         {
+            List<int> my2 = new List<int>();
+            try
+            {
+                Debug.Log("item:" + myitem);
+                using(FileStream fs = new FileStream(myitem, FileMode.Open)){
+                    using(StreamReader sr = new StreamReader(fs)){
+                        Regex r = new Regex("//.*");
+                        string l,t;
+                        string[] s;
+                        int v;
+                        // Read and display lines from the file until the end of
+                        // the file is reached.
+                        while ((l = sr.ReadLine()) != null)
+                        {
+                            //Debug.Log("item l:" + l);
+                            t=r.Replace(l, String.Empty);
+                            //Debug.Log("item t:" + t);
+                            s=t.Split(new char[] {' ','\t',',' });
+                            foreach (var i in s)
+                            {
+                                //Debug.Log("item i:" + i);
+                                if (int.TryParse(i, out v))
+                                {
+                                    Debug.Log("item v:" + v);
+                                    my2.Add(v);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("item:" + e);
+                // throw;
+            }
+            Debug.Log("item my:" + my2.Count);
+            //Debug.Log("item my:" + my2.ToString());
+/*
             int[] my = new int[]
             {
                 131//유틸리티 벨트
@@ -454,12 +678,14 @@ namespace EnterTheGungeon.Lilly.plugins
 				,271//납 어레미
 				,570//노란 약실.저주2
 			};
+            */
+
             PickupObject o;
-            for (int i = 0; i < my.Length; i++)
+            for (int i = 0; i < my2.Count; i++)
             {
                 try
                 {
-                    if ((o = PickupObjectDatabase.GetById(my[i])) != null)
+                    if ((o = PickupObjectDatabase.GetById(my2[i])) != null)
                     {
                         LootEngine.TryGivePrefabToPlayer(o.gameObject, GameManager.Instance.PrimaryPlayer, true);
                     }
@@ -587,6 +813,7 @@ namespace EnterTheGungeon.Lilly.plugins
                         }
                         else
                         {
+                            if (0 < player.passiveItems.Count)
                                 player.DropPassiveItem(player.passiveItems[player.passiveItems.Count - 1]);
                         }
 
